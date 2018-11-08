c77d4bbbb5e48450ee5d1b845b50e21a91719a8f
Sangdon Park <pravusid@gmail.com>
2018-11-08 22:23:18 +0900

    fix: excluding id from dto

--- backend/model/match.go
+++ backend/model/match.go
@@ -2,8 +2,8 @@ package model
 
 type Match struct {
 	BaseModel
-	MatchName string `json:"matchName" gorm:"column:match_name" validate:"required"`
+	MatchName string `json:"matchName" gorm:"unique;column:match_name" validate:"required"`
 	Quota     int    `json:"quota" gorm:"column:quota"`
 	Private   bool   `json:"private" gorm:"column:private"`
-	Available bool   `json:",omitempty" gorm:"column:available;default:0"`
+	Available bool   `json:"-" gorm:"column:available;default:0"`
 }
