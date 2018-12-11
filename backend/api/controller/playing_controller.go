4055b159eb74a6a3d7b1ab8757838293d35f5b22
Sangdon Park <pravusid@gmail.com>
2018-12-11 23:58:53 +0900

    add: the skeleton of playing

--- backend/api/controller/playing_controller.go
+++ backend/api/controller/playing_controller.go
@@ -1,18 +1,45 @@
 package controller
 
 import (
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo"
 	"net/http"
 )
 
-type PlayingController struct{}
+type PlayingController struct {
+	upgrader websocket.Upgrader
+}
 
 func (pc PlayingController) Init(g *echo.Group) {
 	g = g.Group("/play")
 
-	g.GET("", pc.defaultPlay)
+	g.POST("", pc.createPlay)
+	g.GET("/{uuid}", pc.loadPlay)
+
+	pc.upgrader = websocket.Upgrader{}
 }
 
-func (PlayingController) defaultPlay(c echo.Context) error {
+func (pc *PlayingController) createPlay(c echo.Context) error {
 	return c.String(http.StatusOK, "Well played!")
+}
+
+func (pc *PlayingController) loadPlay(c echo.Context) error {
+	ws, err := pc.upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	for {
+		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, World!"))
+		if err != nil {
+			c.Logger().Error(err)
+		}
+
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			c.Logger().Error(err)
+		}
+		c.Logger().Infof("%s\n", msg)
+	}
 }
--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-playground/locales v0.12.1 // indirect
 	github.com/go-playground/universal-translator v0.16.0 // indirect
 	github.com/go-sql-driver/mysql v1.4.0 // indirect
+	github.com/gorilla/websocket v1.4.0
 	github.com/jinzhu/gorm v1.9.1
 	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
 	github.com/joho/godotenv v1.3.0
@@ -15,7 +16,6 @@ require (
 	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/satori/go.uuid v1.2.0
-	github.com/stretchr/objx v0.1.1 // indirect
 	github.com/stretchr/testify v1.2.2
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v0.0.0-20170224212429-dcecefd839c4 // indirect
